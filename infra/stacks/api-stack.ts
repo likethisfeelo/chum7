2d8ac2cdbc865d1e453de72045a249d118a21de7
philobadge <cho.alite@gmail.com>
2026-02-25 14:30:50 +0900

    first-test

--- infra/stacks/api-stack.ts
+++ infra/stacks/api-stack.ts
@@ -22,7 +22,7 @@ export class ApiStack extends Stack {
       allowOrigins:
         stage === 'prod'
           ? ['https://www.chum7.com']
-          : ['http://localhost:5173', 'http://localhost:5174'],
+          : ['http://localhost:5173', 'http://localhost:5174', 'https://test.chum7.com'],
       allowMethods: [
         CorsHttpMethod.GET,
         CorsHttpMethod.POST,
